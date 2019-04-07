a4d66cb46211fd2253036141d7555a659e628eab
MarekK <kozlowskimarek90@gmail.com>
2019-04-07 19:40:34 +0200

    Added JUnit4 test methods for Album class, advanced implementation provided

--- test/music/AlbumTest.java
+++ test/music/AlbumTest.java
@@ -6,54 +6,42 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertSame;
 
 public class AlbumTest {
-    private Song song;
+    private Song newSong;
     private List<Song> albumOfSongs;
+
 
     @Before
     public void setUp() {
-        song = new Song("Random Title", 200);
+        newSong = new Song("Random Title", 200);
         albumOfSongs = new ArrayList<>();
         System.out.println("Set up method executed");
     }
 
     @Test
     public void addNewSongToAlbum() {
-        assertTrue(albumOfSongs.isEmpty());
-        albumOfSongs.add(song);
-        assertFalse(albumOfSongs.isEmpty());
-        System.out.println("method test 'addNewSongToAlbum' completed");
+        albumOfSongs.add(newSong);
+        assertSame(newSong, albumOfSongs.get(0));
+        System.out.println("method test 'addNewSongToAlbum' completed\n***********************************");
     }
 
     @Test
     public void addSongToAlbum() {
-        fail("Not implemented yet");
+        albumOfSongs.add(newSong);
+        assertSame(newSong, albumOfSongs.get(0));
+        System.out.println("method test 'addSongToAlbum' completed\n***********************************");
     }
 
     @Test
     public void isSongExistInAlbum() {
-        fail("Not implemented yet");
-    }
+        albumOfSongs.add(newSong);
+        for (Song song :
+                albumOfSongs) {
 
-    @Test
-    public void printSongsInAlbum() {
-        fail("Not implemented yet");
-    }
-
-    @Test
-    public void getAlbumName() {
-        fail("Not implemented yet");
-    }
-
-    @Test
-    public void getArtistName() {
-        fail("Not implemented yet");
-    }
-
-    @Test
-    public void getAlbumSongs() {
-        fail("Not implemented yet");
+            assertSame(newSong, song);
+        }
+        System.out.println("method test 'isSongExistInAlbum' completed\n***********************************");
     }
 }
